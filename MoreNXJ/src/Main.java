f43259ba3091828b9b5ef1ee6b96f7b934147407
Kris Ebbesen <kve@secoya.dk>
2014-01-30 12:41:38 +0100

    Glorius code

---
+++ MoreNXJ/src/Main.java
@@ -0,0 +1,16 @@
+import lejos.nxt.*;
+import lejos.nxt.comm.Bluetooth;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("JKJK");
+		Button.waitForAnyPress();
+		System.out.println("BTGOOOD");
+		//Bluetooth.setFriendlyName("KRISBOT");
+		Button.waitForAnyPress();
+	}
+
+}
