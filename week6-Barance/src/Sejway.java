8120956169397dc14e39b29e981ce382e21423c3
Kris Ebbesen <kve@secoya.dk>
2014-03-06 15:38:41 +0100

    part 1+2

--- week6-Barance/src/Sejway.java
+++ week6-Barance/src/Sejway.java
@@ -1,112 +1,150 @@
 import static java.lang.Math.signum;
+
+import java.io.DataInputStream;
+
 import lejos.nxt.*;
+import lejos.nxt.comm.BTConnection;
+import lejos.nxt.comm.Bluetooth;
 
 /**
- * A controller for a self-balancing Lego robot with a light sensor
- * on port 2. The two motors should be connected to port B and C.
- *
- * Building instructions in Brian Bagnall: Maximum Lego NXTBuilding 
- * Robots with Java Brains</a>, Chapter 11, 243 - 284
+ * A controller for a self-balancing Lego robot with a light sensor on port 2.
+ * The two motors should be connected to port B and C.
+ * 
+ * Building instructions in Brian Bagnall: Maximum Lego NXTBuilding Robots with
+ * Java Brains</a>, Chapter 11, 243 - 284
  * 
  * @author Brian Bagnall
  * @version 26-2-13 by Ole Caprani for leJOS version 0.9.1
  */
 
+public class Sejway {
 
-public class Sejway 
-{
+	// PID constants
+	volatile int KP = 600; // 600
+	volatile int KI = 80; // 80
+	volatile int KD = 1500; // 600
+	volatile int SCALE = 100;
+	volatile float AJUST = 2.5F;
 
-    // PID constants
-    final int KP = 38;
-    final int KI = 4;
-    final int KD = 33;
-    final int SCALE = 25;
+	// Global vars:
+	int offset;
+	int prev_error;
+	float int_error;
 
-    // Global vars:
-    int offset;
-    int prev_error;
-    float int_error;
-	
-    LightSensor ls;
-	
-    public Sejway() 
-    {
-        ls = new LightSensor(SensorPort.S2, true);
-    }
-	
-    public void getBalancePos() 
-    {
-    	LCD.drawString("BARANCU!", 0, 0);
-        // Wait for user to balance and press orange button
-        while (!Button.ENTER.isDown())
-        {
-        // NXTway must be balanced.
-        offset = ls.readNormalizedValue();
-        LCD.clear();
-        LCD.drawInt(offset, 2, 4);
-        LCD.refresh();
-        }
-    }
-	
-    public void pidControl() 
-    {
-        while (!Button.ESCAPE.isDown()) 
-        {
-            int normVal = ls.readNormalizedValue();
+	LightSensor ls;
 
-            // Proportional Error:
-            int error = normVal - offset;
-            // Adjust far and near light readings:
-            if (error < 0) error = (int)(error * 1.8F);
-			
-            // Integral Error:
-            int_error = ((int_error + error) * 2)/3;
-			
-            if(signum(error)!=signum(prev_error))
-   	    	 int_error = 0;
-            
-            // Derivative Error:
-            int deriv_error = error - prev_error;
-            prev_error = error;
-			
-            
-            int pid_val = (int)(KP * error + KI * int_error + KD * deriv_error) / SCALE;
-			
-            if (pid_val > 100)
-                pid_val = 100;
-            if (pid_val < -100)
-                pid_val = -100;
+	public Sejway() {
+		ls = new LightSensor(SensorPort.S2, true);
+	}
 
-            // Power derived from PID value:
-            int power = Math.abs(pid_val);
-            power = 55 + (power * 45) / 100; // NORMALIZE POWER
-            LCD.clear();
-            LCD.drawInt(power, 0, 0);
-            LCD.drawInt(pid_val, 1, 0);
+	public void getBalancePos() {
+		LCD.drawString("BARANCU!", 0, 0);
+		// Wait for user to balance and press orange button
+		while (!Button.ENTER.isDown()) {
+			// NXTway must be balanced.
+			offset = ls.readNormalizedValue();
+			LCD.clear();
+			LCD.drawInt(offset, 2, 4);
+			LCD.refresh();
+		}
+	}
 
-            if (pid_val < 0) {
-            	MotorPort.B.controlMotor(power, BasicMotorPort.FORWARD);
-                MotorPort.A.controlMotor(power, BasicMotorPort.FORWARD);
-            } else {
-                MotorPort.B.controlMotor(power, BasicMotorPort.BACKWARD);
-                MotorPort.A.controlMotor(power, BasicMotorPort.BACKWARD);
-            }
-        }
-    }
-	
-    public void shutDown()
-    {
-        // Shut down light sensor, motors
-        Motor.B.flt();
-        Motor.C.flt();
-        ls.setFloodlight(false);
-    }
-	
-    public static void main(String[] args) 
-    {
-        Sejway sej = new Sejway();
-        sej.getBalancePos();
-        sej.pidControl();
-        sej.shutDown();
-    }
+	public void pidControl() throws InterruptedException {
+		while (!Button.ESCAPE.isDown()) {
+			int normVal = ls.readNormalizedValue();
+
+			// Proportional Error:
+			int error = normVal - offset;
+			// Adjust far and near light readings:
+			if (error < 0)
+				error = (int) (error * AJUST);
+
+			// Integral Error:
+			int_error = ((int_error + error) * 2) / 3;
+
+			if (signum(error) != signum(prev_error))
+				int_error = 0;
+
+			// Derivative Error:
+			int deriv_error = error - prev_error;
+			prev_error = error;
+
+			int pid_val = (int) (KP * error + KI * int_error + KD * deriv_error)
+					/ SCALE;
+
+			if (pid_val > 100)
+				pid_val = 100;
+			if (pid_val < -100)
+				pid_val = -100;
+
+			// Power derived from PID value:
+			int power = Math.abs(pid_val);
+			power = 55 + (power * 45) / 100; // NORMALIZE POWER
+			LCD.clear();
+			LCD.drawInt(power, 0, 0);
+			LCD.drawInt(pid_val, 0, 1);
+			LCD.drawInt(normVal, 0, 2);
+
+			if (pid_val > 0) {
+				MotorPort.B.controlMotor(power, BasicMotorPort.FORWARD);
+				MotorPort.A.controlMotor(power, BasicMotorPort.FORWARD);
+			} else {
+				MotorPort.B.controlMotor(power, BasicMotorPort.BACKWARD);
+				MotorPort.A.controlMotor(power, BasicMotorPort.BACKWARD);
+			}
+			Thread.sleep(10);
+		}
+	}
+
+	public void shutDown() {
+		// Shut down light sensor, motors
+		Motor.B.flt();
+		Motor.C.flt();
+		ls.setFloodlight(false);
+	}
+
+	public void listen() {
+		new Thread() {
+			public void run() {
+				try {
+					int counter = 0;
+					BTConnection con = Bluetooth.waitForConnection();
+					DataInputStream dis = con.openDataInputStream();
+					while (true) {
+						KP = dis.readInt();
+						KI = dis.readInt();
+						KD = dis.readInt();
+						SCALE = dis.readInt();
+						AJUST = dis.readFloat();
+
+						prev_error = 0;
+						int_error = 0.0f;
+						MotorPort.A.controlMotor(0, BasicMotorPort.FLOAT);
+						MotorPort.B.controlMotor(0, BasicMotorPort.FLOAT);
+						LCD.drawInt(counter++, 0, 0);
+						LCD.drawInt(KP, 0, 1);
+						LCD.drawInt(KI, 0, 2);
+						LCD.drawInt(KD, 0, 3);
+						LCD.drawInt(SCALE, 0, 4);
+
+						Thread.sleep(100);
+					}
+
+				} catch (Throwable t) {
+				}
+			}
+
+			{
+				setDaemon(true);
+			}
+		}.start();
+	}
+
+	public static void main(String[] args) throws Throwable {
+		Sejway sej = new Sejway();
+		sej.listen();
+		sej.getBalancePos();
+		sej.pidControl();
+		sej.shutDown();
+	}
 }
