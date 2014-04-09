f48826f1c6e627b06f2abab2a20389e64dd9958b
Kris Ebbesen <kve@secoya.dk>
2014-04-09 16:45:39 +0200

    WORKS (except distance)

--- week9-Racer/src/DubCar.java
+++ week9-Racer/src/DubCar.java
@@ -6,12 +6,11 @@ import lejos.robotics.navigation.DifferentialPilot;
 import static java.lang.Math.*;
 
 public class DubCar {
-	static NXTRegulatedMotor m1 = new NXTRegulatedMotor(MotorPort.A), m2 = new NXTRegulatedMotor(
-			MotorPort.C);
-	public static void main(String[] aArg) throws Exception {
-		DifferentialPilot2 p = new DifferentialPilot2(8.2, 11.5,
-				new NXTRegulatedMotor(MotorPort.C), new NXTRegulatedMotor(
-						MotorPort.A));
+	static NXTRegulatedMotor m1 = new NXTRegulatedMotor(MotorPort.A),
+			m2 = new NXTRegulatedMotor(MotorPort.C);
+
+	public static void main(String[] aArg) throws Throwable {
+		DifferentialPilot2 p = new DifferentialPilot2(8.2, 11.5, m2, m1);
 		p.setTravelSpeed(20);
 		Button.ENTER.waitForPress();
 		new Thread() {
@@ -29,22 +28,30 @@ public class DubCar {
 				setDaemon(true);
 			}
 		}.start();
-		
-		p.travel(5);
-		p.travelArc(-18.5, 0.01 * PI);
-		p.die();
+
+		// p.travel(5);
+		// p.travelArc(-18.5, 0.01 * PI);
+		// p.die();
 		Linetrack(4000);
+		
+		p.reset();
 		p.travel(10);
-		p.travelArc(-18.5, 19.5 * PI);
+		p.travelArc(-18.5, 19.2 * PI);
 		p.die();
-		Linetrack(m1.getTachoCount()+ m2.getTachoCount()+3800);
+		//m1.resetTachoCount();
+		//m2.resetTachoCount();
+		//p.reset();
+		Linetrack(3800);
 
 	}
 
-	public static void Linetrack(int tacho) {
+	public static void Linetrack(int tacho) throws Throwable {
+		
 		LineFollowerCal.run = true;
+
 		while (!Button.ESCAPE.isDown()) {
 			int count = m1.getTachoCount() + m2.getTachoCount();
+			Thread.sleep(5);
 			LCD.drawInt(count % 100, 0, 0);
 			if (count > tacho) {
 				LineFollowerCal.run = false;
