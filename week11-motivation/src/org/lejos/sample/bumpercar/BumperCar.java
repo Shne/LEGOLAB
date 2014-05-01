c3b6d11007a003d9bffae7428a2f4263a259d1bd
Kris Ebbesen <kve@secoya.dk>
2014-05-01 11:59:59 +0200

    Exit

--- week11-motivation/src/org/lejos/sample/bumpercar/BumperCar.java
+++ week11-motivation/src/org/lejos/sample/bumpercar/BumperCar.java
@@ -14,100 +14,108 @@ import lejos.robotics.subsumption.Behavior;
 /**
  * Demonstration of the Behavior subsumption classes.
  * 
- * Requires a wheeled vehicle with two independently controlled
- * motors connected to motor ports A and C, and 
- * a touch sensor connected to sensor  port 1 and
- * an ultrasonic sensor connected to port 3;
+ * Requires a wheeled vehicle with two independently controlled motors connected
+ * to motor ports A and C, and a touch sensor connected to sensor port 1 and an
+ * ultrasonic sensor connected to port 3;
  * 
  * @author Brian Bagnall and Lawrie Griffiths, modified by Roger Glassey
- *
+ * 
  */
-public class BumperCar
-{
-  static RegulatedMotor leftMotor = Motor.A;
-  static RegulatedMotor rightMotor = Motor.C;
-  
-  // Use these definitions instead if your motors are inverted
-  // static RegulatedMotor leftMotor = MirrorMotor.invertMotor(Motor.A);
-  //static RegulatedMotor rightMotor = MirrorMotor.invertMotor(Motor.C);
-  
-  public static void main(String[] args)
-  {
-    leftMotor.setSpeed(400);
-    rightMotor.setSpeed(400);
-    Behavior b1 = new DriveForward();
-    Behavior b2 = new DetectWall();
-    Behavior[] behaviorList =
-    {
-      b1, b2
-    };
-    Arbitrator arbitrator = new Arbitrator(behaviorList);
-    LCD.drawString("Bumper Car",0,1);
-    Button.waitForAnyPress();
-    arbitrator.start();
-  }
+public class BumperCar {
+	static RegulatedMotor leftMotor = Motor.A;
+	static RegulatedMotor rightMotor = Motor.C;
+
+	// Use these definitions instead if your motors are inverted
+	// static RegulatedMotor leftMotor = MirrorMotor.invertMotor(Motor.A);
+	// static RegulatedMotor rightMotor = MirrorMotor.invertMotor(Motor.C);
+
+	public static void main(String[] args) {
+		leftMotor.setSpeed(400);
+		rightMotor.setSpeed(400);
+		Behavior b1 = new DriveForward();
+		Behavior b2 = new DetectWall();
+		Behavior b3 = new Exit();
+		Behavior[] behaviorList = { b1, b2, b3 };
+		Arbitrator arbitrator = new Arbitrator(behaviorList);
+		LCD.drawString("Bumper Car", 0, 1);
+		Button.waitForAnyPress();
+		arbitrator.start();
+	}
 }
 
+class DriveForward implements Behavior {
 
-class DriveForward implements Behavior
-{
+	private boolean _suppressed = false;
 
-  private boolean _suppressed = false;
+	public boolean takeControl() {
+		return true; // this behavior always wants control.
+	}
 
-  public boolean takeControl()
-  {
-    return true;  // this behavior always wants control.
-  }
+	public void suppress() {
+		_suppressed = true;// standard practice for suppress methods
+	}
 
-  public void suppress()
-  {
-    _suppressed = true;// standard practice for suppress methods
-  }
-
-  public void action()
-  {
-    _suppressed = false;
-    BumperCar.leftMotor.forward();
-    BumperCar.rightMotor.forward();
-    while (!_suppressed)
-    {
-      Thread.yield(); //don't exit till suppressed
-    }
-    BumperCar.leftMotor.stop(); 
-    BumperCar.leftMotor.stop();
-  }
+	public void action() {
+		_suppressed = false;
+		BumperCar.leftMotor.forward();
+		BumperCar.rightMotor.forward();
+		while (!_suppressed) {
+			Thread.yield(); // don't exit till suppressed
+		}
+		BumperCar.leftMotor.stop();
+		BumperCar.leftMotor.stop();
+	}
 }
 
+class DetectWall implements Behavior {
 
-class DetectWall implements Behavior
-{
+	private TouchSensor touch;
+	private TouchSensor touch2;
+	private UltrasonicSensor sonar;
 
-  public DetectWall()
-  {
-    touch = new TouchSensor(SensorPort.S1);
-    sonar = new UltrasonicSensor(SensorPort.S3);
-  }
+	public DetectWall() {
+		touch = new TouchSensor(SensorPort.S1);
+		touch2 = new TouchSensor(SensorPort.S1);
+		sonar = new UltrasonicSensor(SensorPort.S3);
+	}
 
-  public boolean takeControl()
-  {
-    sonar.ping();
-    
-    return touch.isPressed() || sonar.getDistance() < 25;
-  }
+	public boolean takeControl() {
+		sonar.ping();
 
-  public void suppress()
-  {
-    //Since  this is highest priority behavior, suppress will never be called.
-  }
+		return touch.isPressed() || touch2.isPressed()
+				|| sonar.getDistance() < 25;
+	}
 
-  public void action()
-  {
-    BumperCar.leftMotor.rotate(-180, true);// start Motor.A rotating backward
-    BumperCar.rightMotor.rotate(-360);  // rotate C farther to make the turn
-  }
-  
-  private TouchSensor touch;
-  private UltrasonicSensor sonar;
+	public void suppress() {
+		// Since this is highest priority behavior, suppress will never be
+		// called.
+	}
+
+	public void action() {
+		BumperCar.leftMotor.rotate(-180, true);// start Motor.A rotating
+												// backward
+		BumperCar.rightMotor.rotate(-360); // rotate C farther to make the turn
+	}
+	
 }
 
+class Exit implements Behavior {
 
+
+	public Exit() {
+
+	}
+
+	public boolean takeControl() {
+		return Button.ESCAPE.isPressed();
+	}
+
+	public void suppress() {
+		// Since this is highest priority behavior, suppress will never be
+		// called.
+	}
+
+	public void action() {
+		System.exit(0);
+	}
+}
